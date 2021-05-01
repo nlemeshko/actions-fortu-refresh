5625717c3fd3765ddabbff62207902729e2e5d7f
Nicky Lemeshko <admin@mdsn.tk>
2021-05-02 01:32:01 +0300

    Add several args

--- start.sh
+++ start.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 x=1
-while [ $x -le 5 ]; do
-    curl "$TOKEN" > out.json
-    line=$(cat out.json | jq -r '.entries | ."'$CHART'" | .[] | .version' | grep $VERSION)
-    if [ "$line" == "" ]; then
-        echo "Charts no found yet."
-        x=$(( $x + 1 ))
-        sleep 25
-    else
-        echo "Found."
+y=0
+
+while [ $x -le 10 ]; do
+    if [ -z "${CHART[$y]}" ]
+    then
         break
+    else
+        curl "$TOKEN" > out.json
+        line=$(cat out.json | jq -r '.entries | ."'$CHART'" | .[] | .version' | grep $VERSION)
+        if [ "$line" == "" ]; then
+            echo "Charts no found yet."
+            x=$(( $x + 1 ))
+            sleep 25
+        else
+            echo "Found."
+            y=$(( $y + 1 ))
+        fi
     fi
 done
